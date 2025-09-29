f0ae900ae9dd60d746021785a00302d6c8fb6fd9
Ricael <ricaelmenezes@gmail.com>
2025-09-29 12:07:45 -0300

    DepartMentService implemented

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	opens model.entities to javafx.base;
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
     exports gui;
